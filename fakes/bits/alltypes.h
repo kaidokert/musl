1dd3f2dabf54d534b64bbb74b6efcff5e2f59d3b
kaidokert <kaidokert@gmail.com>
2024-11-26 17:46:49 -0800

    Hack a build

---
+++ fakes/bits/alltypes.h
@@ -0,0 +1,22 @@
+
+typedef __SIZE_TYPE__ size_t;
+typedef __SIZE_TYPE__ locale_t;
+// Wrong
+typedef __SIZE_TYPE__ ssize_t;
+// Wrong
+typedef __INTMAX_TYPE__ off_t;
+
+typedef __INT8_TYPE__ int8_t;
+typedef __INT16_TYPE__ int16_t;
+typedef __INT32_TYPE__ int32_t;
+typedef __INT64_TYPE__ int64_t;
+
+typedef __UINT8_TYPE__ uint8_t;
+typedef __UINT16_TYPE__ uint16_t;
+typedef __UINT32_TYPE__ uint32_t;
+typedef __UINT64_TYPE__ uint64_t;
+
+typedef __INTPTR_TYPE__ intptr_t;
+typedef __UINTPTR_TYPE__ uintptr_t;
+
+
